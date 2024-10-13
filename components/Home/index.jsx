d22fe037f54cffbe746ed315f40443e98d0b14ac
Al Emran <alemrangazi@gmail.com>
2024-10-13 19:25:07 +0600

    footer & slider created

--- components/Home/index.jsx
+++ components/Home/index.jsx
@@ -1,7 +1,17 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native";
 
+import image from "../../assets/images/Card Stock01.png";
+import image1 from "../../assets/images/Card Stock02.png";
+import image2 from "../../assets/images/Card Stock02 (1).png";
 const Homepage = () => {
   return (
     <View style={styles.body}>
@@ -13,23 +23,75 @@ const Homepage = () => {
           <Image source={require("../../assets/images/Notification 1.png")} />
         </TouchableOpacity>
       </View>
-      <Text style={styles.welcomeText}>Welcome, Jessie.</Text>
-      <View style={styles.assetCard}>
-        <Text style={styles.yourAssetText}>Your total asset portfolio</Text>
-        <View style={styles.amountContainer}>
-          <Text style={styles.amount}>N203,935</Text>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Invest Now</Text>
-          </TouchableOpacity>
+      <ScrollView style={styles.main}>
+        <Text style={styles.welcomeText}>Welcome, Jessie.</Text>
+        <View style={styles.assetCard}>
+          <Text style={styles.yourAssetText}>Your total asset portfolio</Text>
+          <View style={styles.amountContainer}>
+            <Text style={styles.amount}>N203,935</Text>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Invest Now</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <View style={styles.bestPlan}>
-        <View style={styles.bestPlanText}>
-          <Text style={styles.bestPlanTextLeft}>Best Plans</Text>
-          <TouchableOpacity>
-            <Text style={styles.bestPlanTextRight}>See All &rarr;</Text>
-          </TouchableOpacity>
+        <View style={styles.bestPlan}>
+          <View style={styles.bestPlanText}>
+            <Text style={styles.bestPlanTextLeft}>Best Plans</Text>
+            <TouchableOpacity style={styles.bestPlanRight}>
+              <Text style={styles.bestPlanTextRight}>See All &rarr;</Text>
+            </TouchableOpacity>
+          </View>
+          <ScrollView horizontal={true} style={styles.sliderContainer}>
+            <View style={styles.card}>
+              <ImageBackground
+                source={image}
+                resizeMode="cover"
+                style={styles.bgImage}
+              >
+                <Text style={styles.cardText1}>Gold</Text>
+                <Text style={styles.cardText2}>30% Return</Text>
+              </ImageBackground>
+            </View>
+            <View style={styles.card}>
+              <ImageBackground
+                source={image1}
+                resizeMode="cover"
+                style={styles.bgImage}
+              >
+                <Text style={styles.cardText1}>Silver</Text>
+                <Text style={styles.cardText2}>60% Return</Text>
+              </ImageBackground>
+            </View>
+            <View style={styles.card}>
+              <ImageBackground
+                source={image2}
+                resizeMode="cover"
+                style={styles.bgImage}
+              >
+                <Text style={styles.cardText1}>Platinum</Text>
+                <Text style={styles.cardText2}>90% Return</Text>
+              </ImageBackground>
+            </View>
+          </ScrollView>
         </View>
+      </ScrollView>
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.footerCard}>
+          <Image source={require("../../assets/images/Home 1.png")} />
+          <Text style={styles.footerText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerCard}>
+          <Image source={require("../../assets/images/Search 1.png")} />
+          <Text style={styles.footerText}>Product</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerCard}>
+          <Image source={require("../../assets/images/transaction 1.png")} />
+          <Text style={styles.footerText}>Transaction</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerCard}>
+          <Image source={require("../../assets/images/Home 1.png")} />
+          <Text style={styles.footerText}>Account</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -38,14 +100,15 @@ const Homepage = () => {
 export default Homepage;
 
 const styles = StyleSheet.create({
-  body: { marginTop: 80, marginRight: 15, marginLeft: 15 },
+  body: { marginTop: 40, marginRight: 15, marginLeft: 15 },
+  main: { height: "100%" },
   iconContainer: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    marginBottom: 20,
+    height: 40,
   },
-  welcomeText: { fontSize: 34, fontWeight: 700, lineHeight: 44 },
+  welcomeText: { fontSize: 34, fontWeight: "bold", lineHeight: 44 },
   assetCard: {
     backgroundColor: "#31A078",
     paddingTop: 30,
@@ -60,8 +123,8 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginTop: 10,
   },
-  yourAssetText: { color: "white", fontSize: 14, fontWeight: 500 },
-  amount: { color: "white", fontSize: 30, fontWeight: 600 },
+  yourAssetText: { color: "white", fontSize: 14, fontWeight: "medium" },
+  amount: { color: "white", fontSize: 30, fontWeight: "semibold" },
   button: {
     backgroundColor: "#FEFEFE",
     paddingLeft: 18,
@@ -71,14 +134,51 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     marginRight: 8,
   },
-  buttonText: { color: "#31A078", fontSize: 14, fontWeight: 600 },
+  buttonText: { color: "#31A078", fontSize: 14, fontWeight: "semibold" },
   bestPlan: { paddingTop: 20 },
   bestPlanText: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  bestPlanTextLeft: { fontSize: 22, fontWeight: 700 },
-
-  bestPlanTextRight: { color: "#FE555D" },
+  bestPlanTextLeft: { fontSize: 22, fontWeight: "bold" },
+  bestPlanRight: {
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center",
+  },
+  bestPlanTextRight: { color: "#FE555D", fontSize: 18, fontWeight: "medium" },
+  sliderContainer: { marginTop: 20 },
+  card: { marginRight: 10 },
+  bgImage: {
+    height: 170,
+    width: 134,
+  },
+  cardText1: {
+    fontSize: 17,
+    fontWeight: "semibold",
+    marginLeft: 10,
+    marginTop: 10,
+    color: "white",
+  },
+  cardText2: {
+    fontSize: 13,
+    fontWeight: "semibold",
+    marginLeft: 10,
+    color: "white",
+  },
+  footer: {
+    height: 70,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  footerCard: {
+    width: "25%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  footerText: { color: "#32A078" },
 });
